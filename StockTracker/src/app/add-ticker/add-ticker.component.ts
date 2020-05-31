f4e97287ab5e9b37028a52c488ccf83f463fba06
NAEK2K <naek2k@outlook.com>
2020-05-31 15:32:48 -0400

    created add-ticker component

---
+++ StockTracker/src/app/add-ticker/add-ticker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-ticker',
+  templateUrl: './add-ticker.component.html',
+  styleUrls: ['./add-ticker.component.css']
+})
+export class AddTickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- StockTracker/src/app/app.module.ts
+++ StockTracker/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddTickerComponent } from './add-ticker/add-ticker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTickerComponent
   ],
   imports: [
     BrowserModule
